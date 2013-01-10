98b3243e4089ea76ef2f36518a9d40883828cc57
Daniel Schobel <dschobel@gmail.com>
2013-01-10 23:31:14 +1100

    added proper comments

--- src/main/scala/io/das/huffman/Huffman.scala
+++ src/main/scala/io/das/huffman/Huffman.scala
@@ -2,7 +2,15 @@ package io.das.huffman
 
 object Huffman{
 
-  //repeatedly apply 'fx' until condition 'done' is met
+
+  /**
+   * Repeatedly apply 'fx' until condition 'done' is met
+   * @param data list of values to transform
+   * @param done a predicate on the list of values to indicate when to terminate
+   * @param fx the function to transform the list of values between rounds
+   * @tparam T the type of the values in list of values
+   * @return the transformed list of values
+   */
   def processUntil[T](data: List[T])(done: List[T] => Boolean)(fx: List[T] => List[T]):List[T]={
     if (!done(data)){
       processUntil(fx(data))(done)(fx)
@@ -13,7 +21,13 @@ object Huffman{
   }
 
 
-  //combine list of code tree leaves into an actual tree
+  /**
+   * For code tree lists with more than one elements, combine the first two elements into a fork and
+   * reinsert the fork into list based at the correct position
+   * @param xs the list of code trees
+   * @return return a list containing the new fork
+   *         otherwise return the original list if the original had only one element
+   */
   def combineCodeTrees(xs: List[CodeTree]): List[CodeTree]={
 
     if (xs.length == 1)
@@ -21,7 +35,8 @@ object Huffman{
     else{
 
       val  f = Fork(xs.head,xs.tail.head)//combine two least weighted nodes
-      val (less, greater) = xs.tail.tail.span(_.weight < f.weight) // partition remaining leaves around the new fork's weight
+      val (less, greater) = xs.tail.tail.span(_.weight < f.weight) // partition remaining leaves
+                                                                   // around the new fork's weight
 
       //combine results
       less ::: List(f) ::: greater
@@ -29,28 +44,61 @@ object Huffman{
   }
 
 
-  //generate a code tree from a source of characters
+
+  /**
+   * Generate a code tree reflecting the character frequencies in a source of characters
+   * @param text the character source
+   * @return a Code Tree
+   */
   def makeCodeTreeFromText(text: Iterable[Char]): CodeTree = makeCodeTreeFromFreqs(countFrequency(text))
 
+  /**
+   * Create a code tree from a character frequency map
+   * @param frequencyMap a map of charactes to their frequencies
+   * @return a code tree reflecting the character frequency map
+   */
   def makeCodeTreeFromFreqs(frequencyMap: Map[Char,Int]): CodeTree ={
-
-
     val charsByAscendingFreq = frequencyMap.toList.sortBy({case(c,f) => f}) //sort by ascending frequency
     val leaves = charsByAscendingFreq.map{case(c,f) => Leaf(c,f)} //build a list of leaves
 
-    processUntil[CodeTree](leaves)(_.length == 1)(combineCodeTrees).head //reduce the list into an actual tree structure
+    processUntil[CodeTree](leaves)(_.length == 1)(combineCodeTrees).head //reduce the list into a tree structure
+                                                                         //by repeatedly calling combineCodeTrees
   }
 
 
+  /**
+   * Count the frequency of characters in a characters sequence
+   * @param text source of characters
+   * @return a mapping of characters to frequency counts
+   */
   def countFrequency(text: Iterable[Char]): Map[Char,Int]= text.foldLeft(Map.empty[Char,Int])((map,char) => map + (char -> (map.getOrElse(char,0) + 1)))
 
-  //descends the code tree, chomping off bits until we hit a leaf
-  //returns the character we produced and the remainings bits
+
+
+  /**
+   * Decode a sequence of bits using the provided code tree
+   * @param tree the code tree use to interpret the bits
+   * @param bits the bit stream
+   * @return a sequence of characters representingt the decoded bits
+   */
   def decode(tree: CodeTree, bits: List[Int]): List[Char] = {
+    /**
+     * Descends the code tree, chomping off bits until we hit a leaf
+     * @param tree the code tree
+     * @param bits the sequence of bits
+     * @return the character we produced and the remaining bits
+     */
     def descend(tree: CodeTree, bits: List[Int]): (Char, List[Int]) = tree match {
       case Fork(left, right) => descend(if (bits.head == 0) left else right, bits.tail)
       case Leaf(char, weight) => (char, bits) //we found a leaf node and we're done descending
     }
+
+    /**
+     * control flow method, allowing us to recur until the bit stream is empty
+     * @param bits the sequence of bits to process
+     * @param acc the result accumulator
+     * @return the result sequence of characters
+     */
     def loop(bits: List[Int], acc: List[Char] = Nil): List[Char] = {
       if (bits.isEmpty)
         acc
@@ -63,9 +111,23 @@ object Huffman{
     loop(bits)
   }
 
+  /**
+   * Encodes a character sequence using the provided code tree
+   * @param code the CodeTree used to determine a character's bit-encoding
+   * @param text the character sequence to be encoded
+   * @param acc the result accumulator
+   * @return the sequence of bits representing the encoded text
+   */
   def encode(code: CodeTree, text: Iterable[Char], acc: List[Int] = Nil): List[Int]={
 
-    def getCode(char: Char, root: CodeTree, acc: List[Int] = Nil): List[Int] = root match{
+    /**
+     * Find the bit encoding for a single character
+     * @param char the character to encode
+     * @param node a node of a code tree
+     * @param acc the result accumulator
+     * @return a sequence of bits representing the encoding of the character
+     */
+    def getCode(char: Char, node: CodeTree, acc: List[Int] = Nil): List[Int] = node match{
       case Leaf(_,_) => acc
       case Fork(left,right) => {
         if (left.chars.contains(char))
@@ -74,8 +136,6 @@ object Huffman{
           getCode(char,right, acc ::: List(1))
       }
     }
-
-
 
     if (text.isEmpty)
       acc
